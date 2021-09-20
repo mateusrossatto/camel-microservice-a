cbb211144a0d9e1caecf0dd0cca6936284745ca6
mateus Rossatto <mateusrossatto@gmail.com>
2021-09-19 21:36:23 -0300

    Initial commit

---
+++ camel-microservice-a/src/main/java/com/rossatto/microservices/camelmicroservicea/routes/a/MyFistTimerRouter.java
@@ -0,0 +1,12 @@
+package com.rossatto.microservices.camelmicroservicea.routes.a;
+
+import org.apache.camel.builder.RouteBuilder;
+
+public class MyFistTimerRouter extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+
+    }
+
+}
